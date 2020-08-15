a6efda61fabd37b7b8bd9a5c4739327bc82a713f
Jill Short <jillian.b.short@gmail.com>
2020-08-15 02:54:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
-      self.new = 
+      self.new = student
   end
 
   def add_student_attributes(attributes_hash)
