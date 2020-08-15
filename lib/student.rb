a7e14fd01d2d1840f62b84f3d07154954b699b0c
Jill Short <jillian.b.short@gmail.com>
2020-08-15 03:10:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,13 +18,12 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |attribute|
-      self.send("twitter=", attribute[:twitter])
-      self.send("linkedin=", attribute[:linkedin])
-      self.send("github=", attribute[:github])
-      self.send("blog=", attribute[:blog])
-      self.send("profile_quote=", attribute[:profile_quote])
-      self.send("bio=", attribute[:bio])
+      self.send("twitter=", attributes_hash[:twitter])
+      self.send("linkedin=", attributes_hash[:linkedin])
+      self.send("github=", attributes_hash[:github])
+      self.send("blog=", attributes_hash[:blog])
+      self.send("profile_quote=", attributes_hash[:profile_quote])
+      self.send("bio=", attributes_hash[:bio])
     end
   end
 
